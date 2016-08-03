155047a60cdac85853a407a2f2b3a125c72a5205
Sierra Gregg <sierragregg@Gmail.com>
2016-08-03 15:39:57 -0500

    Finished all GET endpoints

--- server.js
+++ server.js
@@ -12,12 +12,12 @@ app.disable('etag');
 // Middleware suggested by 
 // https://stackoverflow.com/questions/18310394/no-access-control-allow-origin-node-apache-port-issue
 // Add headers
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   // Website you wish to allow to connect
   res.setHeader('Access-Control-Allow-Origin', '*');
 
   // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, PUT');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
 
   // Pass to next layer of middleware
   next();
@@ -25,35 +25,119 @@ app.use(function (req, res, next) {
 
 
 
-app.get('/bookmarks', function (request, response) {
-  getBookmarks().then(function(result){
+app.get('/bookmarks', function(request, response) {
+  getBookmarks().then(function(result) {
     response.json(result.rows);
-  },function(err){
-    response.json(err);
+  }, function(err) {
+    response.status('404').json(err);
   });
 });
 
 app.get('/bookmarks/:folderName', function(request, response) {
   getBookmarks(request.params.folderName).then(function(result) {
     response.json(result.rows);
-  },function(err){
-    response.json(err);
+  }, function(err) {
+    response.status('404').json(err);
   });
 });
 
 app.get('/folder/bookmarks/:folderName', function(request, response) {
   getBookmarks(request.params.folderName).then(function(result) {
     response.json(result.rows);
-  },function(err){
-    response.json(err);
+  }, function(err) {
+    response.status('404').json(err);
   });
 });
 
 app.get('/tag/bookmarks/:tagName', function(request, response) {
   getBookmarks('', request.params.tagName).then(function(result) {
     response.json(result.rows);
-  },function(err){
-    response.json(err);
+  }, function(err) {
+    response.status('404').json(err);
+  });
+});
+
+app.get('/tags', function(request, response) {
+  var connectURL = process.env.DATABASE_URL || 'postgres://localhost:5432/bookmarks';
+  var query = `SELECT tag FROM tag;`;
+  console.log('connectURL: ', connectURL);
+
+  // instantiate a new client
+  var client = new pg.Client(connectURL);
+
+  // connect to database
+  client.connect(function(err) {
+    console.log('client connected');
+    if (err) {
+      console.error(err);
+      response.send("Error " + err);
+    }
+
+    // execute a query on our database
+    client.query(query, function(err, result) {
+      console.log('query results: ', result);
+      if (err) {
+        console.error(err);
+        response.send("Error " + err);
+      }
+
+      var resultsToReturn = result.rows.map(function(value) {
+        return value.tag;
+      });
+
+      // disconnect the client
+      // @question: Is disconnecting neccessary?
+      client.end(function(err) {
+        if (err) {
+          console.error(err);
+          response.send("Error " + err);
+        }
+      });
+
+      response.json(resultsToReturn);
+    });
+  });
+});
+
+app.get('/folders', function(request, response) {
+  var connectURL = process.env.DATABASE_URL || 'postgres://localhost:5432/bookmarks';
+  var query = `SELECT foldername FROM folder;`;
+  console.log('connectURL: ', connectURL);
+
+  // instantiate a new client
+  var client = new pg.Client(connectURL);
+
+  // connect to database
+  client.connect(function(err) {
+    console.log('client connected');
+    if (err) {
+      console.error(err);
+      response.send("Error " + err);
+    }
+
+    // execute a query on our database
+    client.query(query, function(err, result) {
+      console.log('query results: ', result);
+      if (err) {
+        console.error(err);
+        response.send("Error " + err);
+      }
+
+      var resultsToReturn = result.rows.map(function(value) {
+        return value.foldername;
+      });
+
+      // disconnect the client
+      // @question: Is disconnecting neccessary?
+      client.end(function(err) {
+        if (err) {
+          console.error(err);
+          response.send("Error " + err);
+        }
+      });
+
+      response.json(resultsToReturn);
+    });
   });
 });
 
@@ -61,43 +145,48 @@ function getBookmarks(folder, tag) {
   console.log('getbookmarks called with: ', folder, tag);
   var query = `SELECT bookmarkid, url, title, description, foldername, screenshot 
               FROM bookmark`;
-  if(folder) {
-    query = `SELECT bookmarkid, url, title, description, bookmark.foldername, screenshot FROM bookmark JOIN folder ON bookmark.foldername = folder.foldername WHERE folder.foldername = '${folder}';`;
+  if (folder) {
+    query = `SELECT bookmarkid, url, title, description, bookmark.foldername, screenshot 
+            FROM bookmark JOIN folder ON bookmark.foldername = folder.foldername 
+            WHERE folder.foldername = '${folder}';`;
   }
-  if(tag) {
-    query = `SELECT bookmark.bookmarkid, url, title, description, foldername, screenshot, tag FROM bookmark JOIN bookmark_tags ON bookmark.bookmarkid = bookmark_tags.bookmarkid JOIN tag ON bookmark_tags.tagid = tag.tagid;`;
+  if (tag) {
+    query = `SELECT bookmark.bookmarkid, url, title, description, foldername, screenshot, tag 
+            FROM bookmark JOIN bookmark_tags ON bookmark.bookmarkid = bookmark_tags.bookmarkid 
+              JOIN tag ON bookmark_tags.tagid = tag.tagid 
+            WHERE tag.tag = '${tag}';`;
   }
-  console.log('query: ', query);
-  
+  // console.log('query: ', query);
+
   return new Promise(function(resolve, reject) {
     var connectURL = process.env.DATABASE_URL || 'postgres://localhost:5432/bookmarks';
-    console.log('connectURL: ', connectURL);
+    // console.log('connectURL: ', connectURL);
 
     // instantiate a new client
     var client = new pg.Client(connectURL);
 
     // connect to database
-    client.connect(function (err) {
-      console.log('client connected');
-      if (err) { 
-        console.error(err); 
-        response.send("Error " + err); 
+    client.connect(function(err) {
+      // console.log('client connected');
+      if (err) {
+        console.error(err);
+        response.send("Error " + err);
       }
 
       // execute a query on our database
-      client.query(query, function (err, result) {
-        console.log('query results: ', result);
-        if (err) { 
-          console.error(err); 
-          response.send("Error " + err); 
+      client.query(query, function(err, result) {
+        // console.log('query results: ', result);
+        if (err) {
+          console.error(err);
+          response.send("Error " + err);
         }
 
         // disconnect the client
         // @question: Is disconnecting neccessary?
-        client.end(function (err) {
-          if (err) { 
-            console.error(err); 
-            response.send("Error " + err); 
+        client.end(function(err) {
+          if (err) {
+            console.error(err);
+            response.send("Error " + err);
           }
         });
 
