c7c19f0c422bbeff4d52d2c1f9745e099e75b719
Sierra Gregg <sierragregg@Gmail.com>
2016-08-04 16:01:22 -0500

    Ditto above

--- server.js
+++ server.js
@@ -147,7 +147,7 @@ app.post('/bookmark', jsonParser, function(request, response) {
   }
 });
 
-app.post('/folder', jsonParser, function(request, response) {
+app.post('/folder', function(request, response) {
   console.log('app.post:', request.body);
   if(!request.body.foldername) {
     response.status(422).json({
