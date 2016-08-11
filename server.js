1de2f5afd5044750da512caffb321b7709a8a2c0
Robby Kim <robert.st.kim@gmail.com>
2016-08-11 15:27:53 -0400

    changed single quotes to double in access control

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ var app = express();
 var jsonParser = bodyParser.json();
 app.disable('etag');
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET', 'POST', 'PUT', 'DELETE');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET", "POST", "PUT", "DELETE");
   next();
 });
 
