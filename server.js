a797e616c02269f2405cc8f296b96c8adb17bd9b
Robby Kim <robert.st.kim@gmail.com>
2016-08-11 15:52:55 -0400

    added options to allow methods

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.disable('etag');
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "GET", "POST", "PUT", "DELETE");
+  res.header("Access-Control-Allow-Methods", "GET", "POST", "PUT", "DELETE", "OPTIONS");
   next();
 });
 
