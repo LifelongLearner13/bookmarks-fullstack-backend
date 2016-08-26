faf1be0417ea8cad4a426f7e73ab64ff810c057a
Robby Kim <robert.st.kim@gmail.com>
2016-08-25 23:41:10 -0400

    trying to save to sql db

--- test/test-bookmark.js
+++ test/test-bookmark.js
@@ -10,27 +10,37 @@ var should = chai.should();
 
 chai.use(chaiHttp);
 
-describe('Bookmark Endpoints', function () {
-  it('should return an empty list initially', function() {
-    return chai.request(app)
-      .get('/bookmarks')
-      .then(function (res) {
-        res.should.have.status(200);
-        res.type.should.equal('application/json');
-        res.charset.should.equal('utf-8');
-        res.body.should.be.an('array');
-        res.body.length.should.equal(0);
+describe('Bookmark Endpoints', function() {
+  describe('/bookmarks', function() {
+    describe('GET', function() {
+      it('should return an empty list initially', function() {
+        return chai.request(app)
+          .get('/bookmarks')
+          .then(function(res) {
+            res.should.have.status(200);
+            res.type.should.equal('application/json');
+            res.charset.should.equal('utf-8');
+            res.body.should.be.an('array');
+            res.body.length.should.equal(0);
+          });
       });
-  });
-  it('should return an empty list initally for folder', function () {
-    return chai.request(app)
-      .get('/folders')
-      .then(function (res) {
-        res.should.have.status(200);
-        res.type.should.equal('application/json');
-        res.charset.should.equal('utf-8');
-        res.body.should.be.an('array');
-        res.body.length.should.equal(0);
+
+      it('should return a list of bookmarks', function() {
+        var bookmarkA = {
+          url: 'www.url.com',
+          title: 'title',
+          description: 'description',
+          folder: 'folder',
+          screenshot: 'www.screenshot.com',
+        };
+
+        
+        return chai.request(app)
+          .get('/bookmarks')
+          .then(function(res) {
+            res.should.have.status(200);
+          });
       });
+    });
   });
 });
